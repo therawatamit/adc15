11914d00e3609791dfd1f504aa6edcb969305b7c
Amit Rawat <47493778+therawatamit@users.noreply.github.com>
2023-10-28 15:58:58 +0000

    day 4

---
+++ day4b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "md5.h"
+#include <cstring>
+
+using namespace std;
+
+int main(){
+string inp_str = "yzbqklnj";
+string test;
+int i =0;
+while (1){
+    cout<<"current integer:"<<i<<endl;
+    test=inp_str + to_string(i);
+    if(GetMD5String(test).substr(0,6) == "000000"){
+        break;
+    }
+    i++;
+}
+
+cout<<test;
+
+return 0;
+}
